01ea549d520a36d31c68f67fe32d4464b168e1fe
muhammetcnli <howardroark756@gmail.com>
2024-09-30 19:57:13 +0300

    main variables fetched

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 from flask import Flask
+import requests
+import json
+from datetime import datetime
 
-app = Flask(__name__)
+API_KEY = "99cfa13b9efdaad58ab80098082961a5"
+city_name = "Istanbul" # input("Şehir ismi nedir? ")
+
+today = datetime.now()
+day = today.strftime("%A")
+
+LAT_LONG_URL = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={API_KEY}"
+
+for_lat_long = requests.get(LAT_LONG_URL)
+
+data = for_lat_long.json()
+lat = data[0]["lat"]
+lon = data[0]["lon"]
+
+WEATHER_URL = F"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+
+
+r = requests.get(WEATHER_URL)
+
+data_weather = r.json()
+
+hava_durumu = data_weather["weather"][0]["main"]
+
+temp = data_weather["main"]["temp"] - 273.15
+temp = f"{temp:.2f}"
+
+humi = data_weather["main"]["humidity"]
+press = data_weather["main"]["pressure"]
+
+print(day, temp, humi, press)
+
+'''app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)'''
